df3654a8cc214b1400360ba6fd0f835b74c914c5
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2023-07-16 14:18:44 -0400

    Completed the first version

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let rangeValue = document.getElementById('rangeValueInput')
 let numberValue = document.getElementById('numberValue')
+let buttonGenerate = document.querySelector('.generate-button')
 
 rangeValue.addEventListener('input', () => {
   numberValue.textContent = rangeValue.value
+  updateButtonStatus()
 })
 
 //Adding function to copy the password and show the Copied word
@@ -10,41 +12,44 @@ const copyButton = document.getElementById('copy-icon')
 const passwordValue = document.getElementById('password-value')
 const copyWord = document.querySelector('.copied-word')
 
-copyButton.addEventListener('mousedown', () => {
+copyButton.addEventListener('click', () => {
   const textToCopy = passwordValue.value // Obtener el contenido del span
 
-  navigator.clipboard
-    .writeText(textToCopy)
-    .then(() => {
+  if (passwordValue.value) {
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        copyWord.classList.toggle('copied-word--active')
+      })
+      .catch((error) => {
+        console.error('Error al copiar al portapapeles:', error)
+      })
+
+    setTimeout(function () {
       copyWord.classList.toggle('copied-word--active')
-
-      //   setTimeout(function () {
-      //     copyWord.classList.toggle('copied-word--active')
-      //   }, 500)
-    })
-    .catch((error) => {
-      console.error('Error al copiar al portapapeles:', error)
-    })
+    }, 1000)
+  }
 })
 
-copyButton.addEventListener('mouseup', () => {
-  copyWord.classList.toggle('copied-word--active')
+//Update button status when range is 0
+function updateButtonStatus() {
+  if (rangeValue.value === '0')
+    buttonGenerate.classList.add('generate-button--active')
+  else buttonGenerate.classList.remove('generate-button--active')
+}
+updateButtonStatus()
 
-  //   const textToCopy = passwordValue.textContent // Obtener el contenido del span
-
-  //   navigator.clipboard
-  //     .writeText(textToCopy)
-  //     .then(() => {
-  //       copyWord.classList.toggle('copied-word--active')
-
-  //       //   setTimeout(function () {
-  //       //     copyWord.classList.toggle('copied-word--active')
-  //       //   }, 500)
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error al copiar al portapapeles:', error)
-  //     })
+//Change the color when password has no value
+passwordValue.addEventListener('input', () => {
+  updateIconCopyColor()
+  checkStrength()
 })
+
+function updateIconCopyColor() {
+  let iconCopy = document.querySelector('#copy-icon svg path')
+  if (!passwordValue.value) iconCopy.style.fill = '#e6e5ea'
+  else if (passwordValue.value) iconCopy.style.fill = '#a4ffaf'
+}
 
 function generatePassword(passworLenght, options = {}) {
   const {
@@ -59,20 +64,17 @@ function generatePassword(passworLenght, options = {}) {
 
   if (uppercase) characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   if (lowercase) characters += 'abcdefghijklmnopqrstuvwxyz'
-  if (symbols) characters += '!@#$%^&*,.!@#$%^&*,.!@#$%^&*,.'
+  if (symbols) characters += '!@#$%&*,.!@#$%&*,.!@#$%&*,.'
   if (numbers) characters += '012345678901234567890123456789'
 
   for (let i = 0; i < passworLenght; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length)
     password += characters.charAt(randomIndex)
   }
-
   return password
 }
 
 //Add event to the button to create the password
-
-let buttonGenerate = document.querySelector('.generate-button')
 
 buttonGenerate.addEventListener('click', (event) => {
   event.preventDefault()
@@ -88,4 +90,86 @@ buttonGenerate.addEventListener('click', (event) => {
     numbers: numbersElement.checked,
     symbols: symbolsElement.checked,
   })
+  checkStrength()
+  updateIconCopyColor()
 })
+
+function checkStrength() {
+  let password = passwordValue.value
+  setBarsColors(0)
+
+  // const hasUpperCase = /[A-Z]/.test(password)
+  // const hasLowerCase = /[a-z]/.test(password)
+  const hasSymbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)
+  const hasNumbers = /[0-9]/.test(password)
+
+  if (!password) {
+    setBarsColors(0)
+  } else if (password.length < 6) {
+    setBarsColors(0)
+    setBarsColors(1)
+  } else if (password.length >= 6 && password.length <= 8) {
+    if (!hasNumbers && !hasSymbols) {
+      setBarsColors(0)
+      setBarsColors(1)
+    } else {
+      setBarsColors(0)
+      setBarsColors(2)
+    }
+  } else if (password.length === 9) {
+    if (!hasNumbers && !hasSymbols) {
+      setBarsColors(0)
+      setBarsColors(1)
+    } else {
+      setBarsColors(0)
+      setBarsColors(3)
+    }
+  } else {
+    if (!hasNumbers && !hasSymbols) {
+      setBarsColors(0)
+      setBarsColors(1)
+    } else if (!hasNumbers || !hasSymbols) {
+      setBarsColors(0)
+      setBarsColors(3)
+    } else {
+      setBarsColors(0)
+      setBarsColors(4)
+    }
+  }
+}
+
+//Set the category color bars and category background depends on category
+function setBarsColors(categoryNumber) {
+  let bars = document.querySelectorAll('.bar')
+  let strengthCategory = document.getElementById('strengh-category')
+
+  if (categoryNumber === 0) {
+    bars.forEach((bar) => {
+      bar.style.backgroundColor = 'transparent'
+      strengthCategory.textContent = ''
+    })
+  } else if (categoryNumber === 1) {
+    setBarsColors(0)
+    bars[0].style.backgroundColor = '#f64a4a'
+    strengthCategory.textContent = 'too weak!'
+  } else if (categoryNumber === 2) {
+    setBarsColors(0)
+    for (let i = 0; i < 2; i++) {
+      bars[i].style.backgroundColor = '#fb7c58'
+    }
+    strengthCategory.textContent = 'weak!'
+  } else if (categoryNumber === 3) {
+    for (let i = 0; i < 3; i++) {
+      bars[i].style.backgroundColor = '#f8cd65'
+    }
+    strengthCategory.textContent = 'medium'
+  } else if (categoryNumber === 4) {
+    setBarsColors(0)
+    for (let i = 0; i < 4; i++) {
+      bars[i].style.backgroundColor = '#a4ffaf'
+    }
+    strengthCategory.textContent = 'strong'
+  }
+}
+
+checkStrength()
